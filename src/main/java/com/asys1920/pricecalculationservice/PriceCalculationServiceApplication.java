5337696a885ac13b3d87ed59b6b175972d0f3982
Niklas Scherer <scherer-niklas@web.de>
2020-02-26 15:10:22 +0100

    Initial project setup.

---
+++ src/main/java/com/asys1920/pricecalculationservice/PriceCalculationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.asys1920.pricecalculationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceCalculationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PriceCalculationServiceApplication.class, args);
+    }
+}
